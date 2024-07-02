c5cd6c8e475fe2b008292c83144632b27d0679ad
YiYunKung <grace519rm@gmail.com>
2024-07-02 09:41:41 +0800

    221210 hackthon ver

---
+++ store/store/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace store
+{
+    public partial class Form1 : Form
+    {
+        public PictureBox question = new PictureBox();
+        Random Rnd = new Random();
+        int correct = 0;
+        int ans = 0;
+        int Q = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            QA();
+        }
+
+        void QA()
+        {
+            int i = Rnd.Next(1, 10);
+            question.Top = (this.Bottom - question.Height) / 10;
+            question.Left = (this.Width/5);
+            question.Size = new System.Drawing.Size(400, 200);
+            question.SizeMode = PictureBoxSizeMode.StretchImage;
+            question.Image = store.Properties.Resources.Q001;
+            ans = 2;
+            question.Visible = true;
+
+            button1.Visible = true; button2.Visible = true; button3.Visible = true; button4.Visible = true;
+            button1.Top = question.Top + question.Height +15;
+            button2.Top = question.Top + question.Height +15;
+            button3.Top = question.Top + question.Height +15;
+            button4.Top = question.Top + question.Height +15;
+
+            this.Controls.Add(question);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e) //choose1
+        {
+            if (ans == 1)
+            {
+                button1.BackColor = Color.Green;
+                correct++;
+                button5.Visible = true;
+            }
+            else if (ans != 1)
+            {
+                button1.BackColor = Color.Red;
+                if(ans == 2 ) button2.BackColor = Color.Green;
+                if(ans == 3 ) button2.BackColor = Color.Green;
+                if (ans == 4) button2.BackColor = Color.Green;
+                button5.Visible = true;
+            }
+            button1.Enabled = false; button2.Enabled = false; button3.Enabled = false; button4.Enabled = false;
+            label1.Text = "答對 " + correct + " 題";
+        }
+
+        private void button2_Click(object sender, EventArgs e) //choose 2
+        {
+            if (ans == 2)
+            {
+                button2.BackColor = Color.Green;
+                correct++;
+                button5.Visible = true;
+            }
+            else if (ans != 2)
+            {
+                button2.BackColor = Color.Red;
+                if (ans == 1) button1.BackColor = Color.Green;
+                if (ans == 3) button3.BackColor = Color.Green;
+                if (ans == 4) button4.BackColor = Color.Green;
+                button5.Visible = true;
+            }
+            button1.Enabled = false; button2.Enabled = false; button3.Enabled = false; button4.Enabled = false;
+            label1.Text = "答對 " + correct + " 題";
+        }
+
+        private void button3_Click(object sender, EventArgs e) //choose 3
+        {
+            if (ans == 3)
+            {
+                button3.BackColor = Color.Green;
+                correct++;
+                button5.Visible = true;
+            }
+            else if (ans != 3)
+            {
+                button3.BackColor = Color.Red;
+                if (ans == 1) button1.BackColor = Color.Green;
+                if (ans == 2) button2.BackColor = Color.Green;
+                if (ans == 4) button4.BackColor = Color.Green;
+                button5.Visible = true;
+            }
+            button1.Enabled = false; button2.Enabled = false; button3.Enabled = false; button4.Enabled = false;
+            label1.Text = "答對 " + correct + " 題";
+        }
+
+        private void button4_Click(object sender, EventArgs e) //choose 4 
+        {
+            if (ans == 4)
+            {
+                button4.BackColor = Color.Green;
+                correct++;
+                button5.Visible = true;
+            }
+            else if (ans != 4)
+            {
+                button4.BackColor = Color.Red;
+                if (ans == 1) button1.BackColor = Color.Green;
+                if (ans == 2) button2.BackColor = Color.Green;
+                if (ans == 3) button3.BackColor = Color.Green;
+                button5.Visible = true;
+            }
+            button1.Enabled = false; button2.Enabled = false; button3.Enabled = false; button4.Enabled = false;
+            label1.Text = "答對 " + correct + " 題";
+        }
+
+        private void button5_Click(object sender, EventArgs e) //next
+        {
+            if(Q != 10)
+            {
+                Q++;
+                ans = 0;
+                button1.BackColor = Color.White;
+                button2.BackColor = Color.White;
+                button3.BackColor = Color.White;
+                button4.BackColor = Color.White;
+                button1.Enabled = true; button2.Enabled = true; button3.Enabled = true; button4.Enabled = true;
+                button5.Visible = false;
+                nextquestion();
+                //button顏色
+                //enable = true;
+                // ans = 0;
+            }
+            else // Q == 10
+            {
+                MessageBox.Show("共答對 :　" + correct + " 題 \r\n 正確率為 : " + correct*10 + " %", "測驗完成", MessageBoxButtons.OK);
+            }
+        }
+        void nextquestion()
+        {
+            if (Q == 2)
+            {
+                question.Image = store.Properties.Resources.Q002;
+                ans = 4;
+            }
+            if (Q == 3)
+            {
+                question.Image = store.Properties.Resources.Q003;
+                ans = 4;
+            }
+            if (Q == 4)
+            {
+                question.Image = store.Properties.Resources.Q004;
+                ans = 2;
+            }
+            if (Q == 5)
+            {
+                question.Image = store.Properties.Resources.Q005;
+                ans = 2;
+            }
+            if (Q == 6)
+            {
+                question.Image = store.Properties.Resources.Q006;
+                ans = 4;
+            }
+            if (Q == 7)
+            {
+                question.Image = store.Properties.Resources.Q007;
+                ans = 4;
+            }
+            if (Q == 8)
+            {
+                question.Image = store.Properties.Resources.Q008;
+                ans = 3;
+            }
+            if (Q == 9)
+            {
+                question.Image = store.Properties.Resources.Q009;
+                ans = 3;
+            }
+            if (Q == 10)
+            {
+                question.Image = store.Properties.Resources.Q0010;
+                ans = 2;
+            }
+
+        }
+    }
+}
---
+++ video/video/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace video
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(50, 100);
+            this.axWindowsMediaPlayer1.Margin = new System.Windows.Forms.Padding(5);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(980, 650);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.LemonChiffon;
+            this.label1.ForeColor = System.Drawing.Color.IndianRed;
+            this.label1.Location = new System.Drawing.Point(73, 137);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Visible = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 23;
+            this.listBox1.Location = new System.Drawing.Point(1082, 100);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(350, 487);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1924, 843);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
